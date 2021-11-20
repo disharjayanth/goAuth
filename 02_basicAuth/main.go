41715875b1d161cc45689524489581c4c1e2d325
disharjayanth <dishuj15@gmail.com>
2021-11-21 01:26:00 +0530

    added a comment

--- 02_basicAuth/main.go
+++ 02_basicAuth/main.go
@@ -7,5 +7,6 @@ import (
 
 func main() {
 	// Request header container Authoriztion user:pass
+	// Format is Authorization username:password
 	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
 }
