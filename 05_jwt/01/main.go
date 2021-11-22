4458f6b7dd2c6dd7ba83882038b19729cc4f6370
disharjayanth <dishuj15@gmail.com>
2021-11-23 00:56:43 +0530

    jwt create and verify

--- 05_jwt/01/main.go
+++ 05_jwt/01/main.go
@@ -7,6 +7,12 @@ import (
 	"github.com/golang-jwt/jwt"
 )
 
+// private key
+var signKey []byte
+
+// public key
+var verifyKey []byte
+
 type UserClaims struct {
 	jwt.StandardClaims
 	SessionID int64
@@ -24,6 +30,41 @@ func (u *UserClaims) Valid() error {
 	return nil
 }
 
-func main() {
+func createToken(user string) (string, error) {
+	t := jwt.New(jwt.SigningMethodHS256)
 
+	t.Claims = &UserClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(1 * time.Minute).Unix(),
+		},
+		1234,
+	}
+
+	return t.SignedString(signKey)
+}
+
+func verifyToken(token string) {
+	t, err := jwt.ParseWithClaims(token, &UserClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return verifyKey, nil
+	})
+
+	if err != nil {
+		fmt.Println("Error while verifying token:", err)
+		return
+	}
+
+	claims := t.Claims.(*UserClaims)
+	fmt.Println(claims.SessionID)
+}
+
+func main() {
+	token, err := createToken("hello world")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	fmt.Println("Token:", token)
+
+	verifyToken(token)
 }
