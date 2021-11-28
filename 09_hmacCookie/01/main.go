37bbe4301eb369a709e29a7414a26779af139f72
disharjayanth <dishuj15@gmail.com>
2021-11-28 20:12:30 +0530

    setting cookie with hmac

---
+++ 09_hmacCookie/01/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
+)
+
+func createHmacHash(msg string) string {
+	h := hmac.New(sha256.New, []byte("secret string!"))
+	io.WriteString(h, msg)
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func htmlHandler(c *fiber.Ctx) error {
+	cookie := c.Cookies("session")
+
+	return c.Render("index", fiber.Map{
+		"cookie": cookie,
+	})
+}
+
+func cookieHandler(c *fiber.Ctx) error {
+	if c.Method() != http.MethodPost {
+		c.Redirect("/", http.StatusSeeOther)
+		return nil
+	}
+
+	email := c.FormValue("email")
+	if email == "" {
+		c.Redirect("/", http.StatusSeeOther)
+		return nil
+	}
+
+	// hash | what we store
+	hash := createHmacHash(email)
+
+	cookie := &fiber.Cookie{
+		Name:  "session",
+		Value: hash + "|" + email,
+	}
+
+	c.Cookie(cookie)
+
+	return c.Redirect("/", http.StatusSeeOther)
+}
+
+func main() {
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Get("/", htmlHandler)
+	app.Post("/submit", cookieHandler)
+
+	log.Fatal(app.Listen(":8000"))
+}
