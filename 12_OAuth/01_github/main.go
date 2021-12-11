31a0876ace95bf87fac785fb66f8f7c9474d073e
disharjayanth <dishuj15@gmail.com>
2021-12-11 19:29:47 +0530

    OAuth with github completed

--- 12_OAuth/01_github/main.go
+++ 12_OAuth/01_github/main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html"
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/github"
 )
 
+// Key is githubID, value is userID
+var gitHubConnections = make(map[string]string)
+
 var githubOAuthConfig = &oauth2.Config{}
+
+// JSON Layout: {"data":{"viewer":{"id":".."}}}
+type GitHubResponse struct {
+	Data struct {
+		Viewer struct {
+			ID string `json:"id"`
+		} `json:"viewer"`
+	} `json:"data"`
+}
 
 func intialPageHandler(c *fiber.Ctx) error {
 	return c.Render("index", fiber.Map{
@@ -50,7 +65,36 @@ func callBackCodeHandler(c *fiber.Ctx) error {
 
 	client := oauth2.NewClient(c.Context(), tokenSource)
 
-	return nil
+	const query = `{ 
+		"query": "query { viewer { id }}" 
+	}`
+
+	queryReader := strings.NewReader(query)
+	resp, err := client.Post("https://api.github.com/graphql", "application/json", queryReader)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	var githubResponse GitHubResponse
+	err = json.NewDecoder(resp.Body).Decode(&githubResponse)
+	if err != nil {
+		return err
+	}
+
+	gitHubID := githubResponse.Data.Viewer.ID
+	userID, ok := gitHubConnections[gitHubID]
+	if !ok {
+		// new user create account
+		// depends on project
+		gitHubConnections[gitHubID] = uuid.NewString()
+		return c.SendString(gitHubConnections[gitHubID])
+	}
+
+	fmt.Println("userID:", githubResponse)
+	// Login user and send JWT
+
+	return c.SendString(userID)
 }
 
 func main() {
